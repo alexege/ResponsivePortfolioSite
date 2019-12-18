6bae389c328f0017ff86c35f4c1923ceead103a9
Alexander Ege <alexege@gmail.com>
2019-12-18 12:36:47 -0800

    Added responsive social media navs

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+$(document).on('mouseenter', '.trigger-parent', function(){
+    var name = this.querySelector('.trigger').getAttribute('name');
+    console.log("this:", this.getAttribute('name'));
+    document.querySelector(`.${name}-slider`).style.display = "inline-block";
+    this.querySelector('a').style.marginLeft = '1rem';
+    // this.querySelector('span').style.color = "#0ffbf9";
+    this.style.borderLeft = '2px solid #0ffbf9';
+});
+
+$(document).on('mouseleave', '.trigger-parent', function(){
+    var name = this.querySelector('.trigger').getAttribute('name');
+    console.log("this:", this.getAttribute('name'));
+    document.querySelector(`.${name}-slider`).style.display = "none";
+    this.querySelector('a').style.marginLeft = '0rem';
+    this.style.borderLeft = 'none';
+});
+
+function showLink(){
+    document.getElementsByClassName('linkedin-slider')[0].height = '0px';
+}
+
 function setHexOpacity(){
     var hexagons = document.getElementsByClassName('hexagon');
     console.log("hexagon:", hexagons[0])
@@ -18,154 +39,82 @@ function setHexOpacity(){
     }
 }
 
+function fadeOutEnterButton(){
+    document.getElementsByClassName('enter_hex')[0].classList.add('fade');
+    setTimeout(function(){
+        var enter_hex = document.getElementsByClassName('enter_hex')[0];
+        fade(enter_hex);
+    }, 1500);
+}
+
 $(document).on('mouseover', '.hexagon', function(){
     console.log("Entering a hexagon");
     this.classList.add('fade');
-})
+});
 
-
-// setInterval(function(){
-//     var random_hex_id = parseInt(Math.random()*113);
-//     var alpha = Math.random();
-//     console.log("random number:", random_hex_id);
-//     console.log("alpha:", alpha);
-
-//     // document.getElementsByClassName(`hex${random_hex_id}`)[0].classList.toggle('filled');
-//     document.getElementsByClassName(`hex${random_hex_id}`)[0].style.fill = `rgb(6, 174, 226, ${alpha})`;
-//     document.getElementsByClassName(`hex${random_hex_id}`)[0].classList.toggle('fade');
-// }, 100)
-
-setInterval(function(){
-    var random_hex_id = parseInt(Math.random()*113);
-    // document.getElementsByClassName(`hex${random_hex_id}`)[0].classList.toggle('filled');
-    document.getElementsByClassName(`hex${random_hex_id}`)[0].classList.toggle('fade');
-}, 500)
-
-// setInterval(function(){
-//     var random_hex_id1 = parseInt(Math.random()*113);
-//     var random_hex_id2 = parseInt(Math.random()*113);
-//     var random_hex_id3 = parseInt(Math.random()*113);
-//     console.log("random number:", random_hex_id1);
-//     console.log("random number:", random_hex_id2);
-//     console.log("random number:", random_hex_id3);
-//     document.getElementsByClassName(`hex${random_hex_id1}`)[0].classList.toggle('faded');
-//     document.getElementsByClassName(`hex${random_hex_id2}`)[0].classList.toggle('faded');
-//     document.getElementsByClassName(`hex${random_hex_id3}`)[0].classList.toggle('faded');
-// }, 400)
+function randomFadeInOut(){
+    setInterval(function(){
+        var random_hex_id = parseInt(Math.random()*113);
+        document.getElementsByClassName(`hex${random_hex_id}`)[0].classList.toggle('fade');
+    }, 500);
+}
 
 function Enter(){
-    console.log("Running Enter function.");
+    // Grab all hexagons on the page
     var hexagons = document.getElementsByClassName('hexagon');
     
+    // Loop through each hexagon and remove fade effect.
     for(var i = 0; i < hexagons.length; i++){
         hexagons[i].classList.remove('fade');
     }
+
+    // Scale up all hexagons to start transition
+    for(var i = 0; i < hexagons.length; i++){
+        scale(hexagons[i]);
+    }
+
+    // After 750 milliseconds, fade the scaled up hexagons to black
     setTimeout(function(){
         for(var i = 0; i < hexagons.length; i++){
-            
-            scale(hexagons[i]);
-            // fade(hexagons[i]);
+            fade(hexagons[i]);
+            fadeOutEnterButton();
         }
-    });
-    
+    }, 750)
 
     setTimeout(function(){
-        for(var i = 0; i < hexagons.length/2; i++){
-            fade(hexagons[i]);
-        }
-    })
-
-    // for(var i = 0; i < 7; i++){
-    //     scale(hexagons[i]);
-    //     fade(hexagons[i]);
-    // }
-
-    // setTimeout(function(){
-    //     for(var i = 7; i < 19; i++){
-    //         scale(hexagons[i]);
-    //         fade(hexagons[i]);
-    //     }
-    // },250)
-
-    // setTimeout(function(){
-    //     for(var i = 19; i < 38; i++){
-    //         scale(hexagons[i]);
-    //         fade(hexagons[i]);
-    //     }
-    // },500)
-
-    // setTimeout(function(){
-    //     for(var i = 38; i < 64; i++){
-    //         scale(hexagons[i]);
-    //         fade(hexagons[i]);
-    //     }
-    // },750)
-
-    // setTimeout(function(){
-    //     for(var i = 64; i < hexagons.length; i++){
-    //         scale(hexagons[i]);
-    //         fade(hexagons[i]);
-    //     }
-    // },1000)
+        window.location.href = '/about.html';
+    }, 2000);
+    onIndexPage = false;
 }
 
-    function fade(element) {
-        var op = 1;  // initial opacity
-        var timer = setInterval(function () {
-            if (op <= 0.1){
-                clearInterval(timer);
-                element.style.display = 'none';
-            }
-            element.style.opacity = op;
-            element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-            op -= op * 0.1;
-        }, 50);
-    }
+function fade(element) {
+    var op = 1;  // initial opacity
+    var timer = setInterval(function () {
+        if (op <= 0.1){
+            clearInterval(timer);
+            element.style.display = 'none';
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.1;
+    }, 50);
+}
 
-    function scale(element) {
-        // element.style.transform = "scale(0.5)";
-        setTimeout(function(){
-            element.style.transform = "scale(2.0)";
-        }, 50);
-        // setTimeout(function(){
-        //     element.style.transform = "scale(1.0)";
-        // }, 1000);
-    }
-    // setInterval(function(){
-    //     for(var i = 7; i < 19; i++){
+function scale(element) {
+    setTimeout(function(){
+        element.style.transform = "scale(2.0)";
+    }, 50);
+}
 
-    //         hexagons[i].style.opacity = 0;
-    //     }
-    // }, 400);
-    // setInterval(function(){
-    //     for(var i = 19; i < 38; i++){
-
-    //         hexagons[i].style.opacity = 0;
-    //     }
-    // }, 800);
-    // setInterval(function(){
-    //     for(var i = 38; i < 64; i++){
-
-    //         hexagons[i].style.opacity = 0;
-    //     }
-    // }, 1000);
-    // setInterval(function(){
-    //     for(var i = 64; i < hexagons.length; i++){
-
-    //         hexagons[i].style.opacity = 0;
-    //     }
-    // }, 1200);
-    // for(var i = 0; i < hexagons.length; i++){
-    //     hexagons[i].style.opacity = 0;
-    // }
-// }
-
-// Clicking on a hexagon
+// Clicking on a hexagon adds the filled class
 $(document).on('click', '.hexagon', function(){
     this.classList.toggle('filled');
 });
 
 $(document).ready(function(){
     console.log("Document is ready.");
+    //Set all hexagons to a starting opacity
     setHexOpacity();
+    randomFadeInOut();
+
 })

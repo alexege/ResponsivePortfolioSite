9a5ae47e9d813293352f38b652d9112337bc1955
Alexander Ege <alexege@gmail.com>
2020-01-06 11:58:18 -0800

    Fixed issue of script duplicating actions

---
+++ about_script.js
@@ -0,0 +1,95 @@
+$(document).ready(function(){
+    typewriter();
+});
+
+$(document).on('mouseenter', 'a', function(e){
+
+    captureMousePosition(e);
+
+    // console.log("Mouse positionX: ", e.clientX);
+    // console.log("Mouse positionY: ", e.clientY);
+
+    document.getElementById('follow').style.left = `${xMousePos - 25}px`;
+    document.getElementById('follow').style.top = `${yMousePos - 25}px`;
+
+    document.getElementById('corner-top-left').style.borderLeft = '4px solid cyan';
+    document.getElementById('corner-top-left').style.borderTop = '4px solid cyan';
+    document.getElementById('corner-top-right').style.borderRight = '4px solid cyan';
+    document.getElementById('corner-top-right').style.borderTop = '4px solid cyan';
+    document.getElementById('corner-bottom-left').style.borderLeft = '4px solid cyan';
+    document.getElementById('corner-bottom-left').style.borderBottom = '4px solid cyan';
+    document.getElementById('corner-bottom-right').style.borderRight = '4px solid cyan';
+    document.getElementById('corner-bottom-right').style.borderBottom = '4px solid cyan';
+})
+
+$(document).on('mouseleave', 'a', function(e){
+
+    document.getElementById('corner-top-left').style.borderLeft = '2px solid white';
+    document.getElementById('corner-top-left').style.borderTop = '2px solid white';
+    document.getElementById('corner-top-right').style.borderRight = '2px solid white';
+    document.getElementById('corner-top-right').style.borderTop = '2px solid white';
+    document.getElementById('corner-bottom-left').style.borderLeft = '2px solid white';
+    document.getElementById('corner-bottom-left').style.borderBottom = '2px solid white';
+    document.getElementById('corner-bottom-right').style.borderRight = '2px solid white';
+    document.getElementById('corner-bottom-right').style.borderBottom = '2px solid white';
+})
+
+function typewriter(){
+    // TypeWriter Effect
+    // List of sentences
+    var _CONTENT = [ "Web Developer", "Computer Engineer", "Unicyclist", "Software Developer"];
+
+    // Current sentence being processed
+    var _PART = 0;
+    
+    // Character number of the current sentence being processed 
+    var _PART_INDEX = 0;
+    
+    // Holds the handle returned from setInterval
+    var _INTERVAL_VAL;
+    
+    // Element that holds the text
+    var _ELEMENT = document.querySelector("#text");
+    
+    // Implements typing effect
+    function Type() { 
+        var text =  _CONTENT[_PART].substring(0, _PART_INDEX + 1);
+        _ELEMENT.innerHTML = text;
+        _PART_INDEX++;
+    
+        // If full sentence has been displayed then start to delete the sentence after some time
+        if(text === _CONTENT[_PART]) {
+            clearInterval(_INTERVAL_VAL);
+            setTimeout(function() {
+                _INTERVAL_VAL = setInterval(Delete, 50);
+            }, 1000);
+        }
+    }
+    
+    // Implements deleting effect
+    function Delete() {
+        var text =  _CONTENT[_PART].substring(0, _PART_INDEX - 1);
+        _ELEMENT.innerHTML = text;
+        _PART_INDEX--;
+    
+        // If sentence has been deleted then start to display the next sentence
+        if(text === '') {
+            clearInterval(_INTERVAL_VAL);
+    
+            // If last sentence then display the first one, else move to the next
+            if(_PART == (_CONTENT.length - 1))
+                _PART = 0;
+            else
+                _PART++;
+            _PART_INDEX = 0;
+    
+            // Start to display the next sentence after some time
+            setTimeout(function() {
+                _INTERVAL_VAL = setInterval(Type, 100);
+            }, 800);
+        }
+    }
+    
+    // Start the typing effect on load
+    _INTERVAL_VAL = setInterval(Type, 150);
+}
--- script.js
+++ script.js
@@ -29,81 +29,36 @@ function captureMousePosition(event){
 }
 
 var boxes = document.getElementsByClassName('education-box');
-console.log("Boxes:", boxes);
-
 var current_educationbox = 'dojo';
 var category = 'dojo';
 
 $(document).on('mouseenter', '.education-box', function(e){
     category = this.getAttribute('category');
-    console.log("category:", category);
-
     // document.getElementById(`${category}_content`).style.display = "block";
     current_educationbox = `${category}_content`;
 })
-
-$(document).on('mouseleave', '.education-box', function(e){
-    console.log('current:', current_educationbox);
-});
 
 $(document).on('click', '.education-box', function(e){
     this.classList.toggle('active_edu');
     // this.style.border = "2px solid red";
     var list_of_active = document.getElementById('skills-container').querySelectorAll('.active_edu');
     for(var i = 0; i < list_of_active.length; i++){
-        console.log("item:", list_of_active[i]);
         if(list_of_active[i].getAttribute('category') != category){
             list_of_active[i].classList.toggle('active_edu');
         }
     }
     var list_of_visible = document.getElementById('skills-container').querySelectorAll('.info');
     for(var i = 0; i < list_of_visible.length; i++){
-        console.log("Items:", list_of_visible[i]);
         if(list_of_visible[i].classList.contains('visible')){
-            console.log("Visible found");
             list_of_visible[i].classList.toggle('visible');
         }
     }
     // document.getElementById(current_educationbox).style.display = 'block';
     document.getElementById(`${category}_content`).classList.toggle('visible');
-    console.log("list of active: ", list_of_active);
 });
-
-$(document).on('mouseenter', 'a', function(e){
-
-    captureMousePosition(e);
-
-    console.log("Mouse positionX: ", e.clientX);
-    console.log("Mouse positionY: ", e.clientY);
-
-    document.getElementById('follow').style.left = `${xMousePos - 25}px`;
-    document.getElementById('follow').style.top = `${yMousePos - 25}px`;
-
-    document.getElementById('corner-top-left').style.borderLeft = '2px solid cyan';
-    document.getElementById('corner-top-left').style.borderTop = '2px solid cyan';
-    document.getElementById('corner-top-right').style.borderRight = '2px solid cyan';
-    document.getElementById('corner-top-right').style.borderTop = '2px solid cyan';
-    document.getElementById('corner-bottom-left').style.borderLeft = '2px solid cyan';
-    document.getElementById('corner-bottom-left').style.borderBottom = '2px solid cyan';
-    document.getElementById('corner-bottom-right').style.borderRight = '2px solid cyan';
-    document.getElementById('corner-bottom-right').style.borderBottom = '2px solid cyan';
-})
-
-$(document).on('mouseleave', 'a', function(e){
-
-    document.getElementById('corner-top-left').style.borderLeft = '2px solid white';
-    document.getElementById('corner-top-left').style.borderTop = '2px solid white';
-    document.getElementById('corner-top-right').style.borderRight = '2px solid white';
-    document.getElementById('corner-top-right').style.borderTop = '2px solid white';
-    document.getElementById('corner-bottom-left').style.borderLeft = '2px solid white';
-    document.getElementById('corner-bottom-left').style.borderBottom = '2px solid white';
-    document.getElementById('corner-bottom-right').style.borderRight = '2px solid white';
-    document.getElementById('corner-bottom-right').style.borderBottom = '2px solid white';
-})
 
 function openProjectModal(project){
     var selected_project = document.getElementById(project);
-    console.log("Opening modal for " + project + "_nav");
     document.getElementById(`${project}_nav`).style.width = "100%";
 }
 
@@ -132,7 +87,6 @@ function showLink(){
 
 function setHexOpacity(){
     var hexagons = document.getElementsByClassName('hexagon');
-    console.log("hexagon:", hexagons[0])
     for(var i = 0; i < 7; i++){
         hexagons[i].style.opacity = 1.0;
     }
@@ -160,13 +114,15 @@ function fadeOutEnterButton(){
 
 $(document).on('mouseover', '.hexagon', function(e){
     this.querySelector('.hex').classList.toggle('fade');
-    console.log("Entering a hexagon");
     // this.classList.add('fade');
 });
 
 function randomFadeInOut(){
     setInterval(function(){
         var random_hex_id = parseInt(Math.random()*113);
+        if(random_hex_id == 0){
+            random_hex_id = 1;
+        }
         document.getElementsByClassName(`hex${random_hex_id}`)[0].classList.toggle('fade');
     }, 500);
 }
@@ -208,13 +164,8 @@ $(document).on('click', '#about_button', function(){
 });
 
 function aboutPageTrigger(){
-    console.log("It's working!");
     document.getElementsByClassName('hexagon')[0].style.opacity = 0;
-    var hexagons = document.getElementsByClassName('hexagon');
-    // for(var i = 0; i < hexagons.length; i++) {
-    //     hexagons[i].style.position = 'fixed';
-    // }
-    
+    var hexagons = document.getElementsByClassName('hexagon');    
     document.getElementsByClassName('hexagon')[0].style.opacity = 0;
 }
 
@@ -237,77 +188,15 @@ function scale(element) {
     }, 50);
 }
 
-function typewriter(){
-    // TypeWriter Effect
-    // List of sentences
-    var _CONTENT = [ "Web Developer", "Computer Engineer", "Unicyclist", "Software Developer"];
-
-    // Current sentence being processed
-    var _PART = 0;
-    
-    // Character number of the current sentence being processed 
-    var _PART_INDEX = 0;
-    
-    // Holds the handle returned from setInterval
-    var _INTERVAL_VAL;
-    
-    // Element that holds the text
-    var _ELEMENT = document.querySelector("#text");
-    
-    // Implements typing effect
-    function Type() { 
-        var text =  _CONTENT[_PART].substring(0, _PART_INDEX + 1);
-        _ELEMENT.innerHTML = text;
-        _PART_INDEX++;
-    
-        // If full sentence has been displayed then start to delete the sentence after some time
-        if(text === _CONTENT[_PART]) {
-            clearInterval(_INTERVAL_VAL);
-            setTimeout(function() {
-                _INTERVAL_VAL = setInterval(Delete, 50);
-            }, 1000);
-        }
-    }
-    
-    // Implements deleting effect
-    function Delete() {
-        var text =  _CONTENT[_PART].substring(0, _PART_INDEX - 1);
-        _ELEMENT.innerHTML = text;
-        _PART_INDEX--;
-    
-        // If sentence has been deleted then start to display the next sentence
-        if(text === '') {
-            clearInterval(_INTERVAL_VAL);
-    
-            // If last sentence then display the first one, else move to the next
-            if(_PART == (_CONTENT.length - 1))
-                _PART = 0;
-            else
-                _PART++;
-            _PART_INDEX = 0;
-    
-            // Start to display the next sentence after some time
-            setTimeout(function() {
-                _INTERVAL_VAL = setInterval(Type, 100);
-            }, 800);
-        }
-    }
-    
-    // Start the typing effect on load
-    _INTERVAL_VAL = setInterval(Type, 150);
-}
-
 // Clicking on a hexagon adds the filled class
-$(document).on('click', '.hexagon', function(){
-    this.classList.toggle('filled');
+$(document).on('mousedown', '.hexagon', function(){
+    this.classList.toggle("filled");
 });
 
 $(document).ready(function(){
-    console.log("Document is ready.");
     //Set all hexagons to a starting opacity
     setHexOpacity();
     randomFadeInOut();
-    typewriter();
 })
 
 window.onscroll = function() {
